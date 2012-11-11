bb67e698b9b28509a6a14174294c6c69a2a0e2b5
Aaron Krebs <akrebs@ualberta.ca>
2012-11-10 18:19:31 -0700

    Refactored login into own app.
    This should allow for better modularization in future and cleaner.
    Common models and stuff currently in budget app will be moved to
    'shared'app.

--- finances/finances/urls.py
+++ finances/finances/urls.py
@@ -5,9 +5,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^budget/', include('budget.urls', namespace = 'budget', app_name = 'budget')),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/', include('register.urls', namespace = 'register', app_name = 'register')),
+    url(r'^budget/', include('budget.urls', namespace = 'budget', app_name = 'budget')),
 )
