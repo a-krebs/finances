6be8638d7403da30aab835defe94ff689d84e080
Aaron Krebs <akrebs@ualberta.ca>
2012-11-17 18:43:20 -0700

    Site content containers implemented.
    Used hamlpy and sass to implement site's base container layout. Hamlpy
    blocks can be used to extend site_base.hamlpy.
    Sass and scss used for css generation, and scss accordingly added to
    requirements. Also, settings and urls updated to serve up static files
    using context preprocessor and app-specific static directories.
    This should be changed to a more permanent solution before deployment.

--- finances/finances/urls.py
+++ finances/finances/urls.py
@@ -4,6 +4,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+# development serving of static files
+# TODO: before deployment, server static files properly
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
@@ -11,3 +15,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/', include('register.urls', namespace = 'register', app_name = 'register')),
 )
+
+# TODO: remove before deployment
+urlpatterns += staticfiles_urlpatterns()
