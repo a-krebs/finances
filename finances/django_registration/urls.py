e0260e8a917e11672c843d677dd1b92392571007
Aaron Krebs <akrebs@ualberta.ca>
2012-11-22 20:41:24 -0700

    Added remaining registration views to urlconf, and their templates.

--- finances/django_registration/urls.py
+++ finances/django_registration/urls.py
@@ -16,8 +16,9 @@
 from django.views.generic.simple import direct_to_template
 
 from registration.views import register
+from django.contrib.auth import views as auth_views
 
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     # urls for simple one-step registration
     url(r'^register/$',
@@ -32,5 +33,44 @@ urlpatterns = patterns('',
         {'template': 'registration/registration_closed.hamlpy'},
         name='registration_disallowed'
     ),
-    (r'', include('registration.auth_urls')),
+    url(r'^login/$',
+        auth_views.login,
+        {'template_name': 'registration/login.hamlpy'},
+        name='auth_login'
+    ),
+    url(r'^logout/$',
+        auth_views.logout,
+        {'template_name': 'registration/logout.hamlpy'},
+        name='auth_logout'
+    ),
+    url(r'^password/change/$',
+        auth_views.password_change,
+        {'template_name' : 'registration/password_change_form.hamlpy'},
+        name='auth_password_change'
+    ),
+    url(r'^password/change/done/$',
+        auth_views.password_change_done,
+        {'template_name' : 'registration/password_change_done.hamlpy'},
+        name='auth_password_change_done'
+    ),
+    url(r'^password/reset/$',
+        auth_views.password_reset,
+        {'template_name' : 'registration/password_reset_form.hamlpy'},
+        name='auth_password_reset'
+    ),
+    url(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        auth_views.password_reset_confirm,
+        {'template_name' : 'registration/password_reset_confirm.hamlpy'},
+        name='auth_password_reset_confirm'
+    ),
+    url(r'^password/reset/complete/$',
+        auth_views.password_reset_complete,
+        {'template_name' : 'registration/password_reset_complete.hamlpy'},
+        name='auth_password_reset_complete'
+    ),
+    url(r'^password/reset/done/$',
+        auth_views.password_reset_done,
+        {'template_name' : 'registration/password_reset_done.hamlpy'},
+        name='auth_password_reset_done'
+    ),
 )
