57e62684f770bbcfb487e48360a783ae1014715d
Aaron Krebs <akrebs@ualberta.ca>
2013-03-29 16:30:21 -0600

    Formatting for conventions compliance.

--- finances/django_registration/urls.py
+++ finances/django_registration/urls.py
@@ -25,7 +25,7 @@ urlpatterns = patterns('',
     url(r'^register/$',
         register,
         {'backend': 'registration.backends.simple.SimpleBackend',
-            'template_name' : 'registration/registration_form.hamlpy',
+            'template_name': 'registration/registration_form.hamlpy',
         },
         name='registration_register'
     ),
@@ -46,45 +46,45 @@ urlpatterns = patterns('',
     ),
     url(r'^password/change/$',
         auth_views.password_change,
-        {'template_name' : 'registration/password_change_form.hamlpy',
+        {'template_name': 'registration/password_change_form.hamlpy',
             # ugh, this is tied to the namespace; needs to be namespace-agnostic
             # since the namspace is determined by the importing app
             # TODO: see Issue #1
-            'post_change_redirect' : reverse_lazy('registration:auth_password_change_done')
+            'post_change_redirect': reverse_lazy('registration:auth_password_change_done')
         },
         name='auth_password_change'
     ),
     url(r'^password/change/done/$',
         auth_views.password_change_done,
-        {'template_name' : 'registration/password_change_done.hamlpy'},
+        {'template_name': 'registration/password_change_done.hamlpy'},
         name='auth_password_change_done'
     ),
     url(r'^password/reset/$',
         auth_views.password_reset,
-        {'template_name' : 'registration/password_reset_form.hamlpy',
+        {'template_name': 'registration/password_reset_form.hamlpy',
             # same issue as above
-            'post_reset_redirect' : reverse_lazy('registration:auth_password_reset_done'),
-            'email_template_name' : 'registration/password_reset_email.hamlpy',
-            'subject_template_name' : 'registration/password_reset_subject.hamlpy',
+            'post_reset_redirect': reverse_lazy('registration:auth_password_reset_done'),
+            'email_template_name': 'registration/password_reset_email.hamlpy',
+            'subject_template_name': 'registration/password_reset_subject.hamlpy',
         },
         name='auth_password_reset'
     ),
     url(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
         auth_views.password_reset_confirm,
-        {'template_name' : 'registration/password_reset_confirm.hamlpy',
+        {'template_name': 'registration/password_reset_confirm.hamlpy',
             # same issue as above
-            'post_reset_redirect' : reverse_lazy('registration:auth_password_reset_complete'),
+            'post_reset_redirect': reverse_lazy('registration:auth_password_reset_complete'),
         },
         name='auth_password_reset_confirm'
     ),
     url(r'^password/reset/complete/$',
         auth_views.password_reset_complete,
-        {'template_name' : 'registration/password_reset_complete.hamlpy'},
+        {'template_name': 'registration/password_reset_complete.hamlpy'},
         name='auth_password_reset_complete'
     ),
     url(r'^password/reset/done/$',
         auth_views.password_reset_done,
-        {'template_name' : 'registration/password_reset_done.hamlpy'},
+        {'template_name': 'registration/password_reset_done.hamlpy'},
         name='auth_password_reset_done'
     ),
 )
--- finances/finances/urls.py
+++ finances/finances/urls.py
@@ -28,8 +28,8 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^register/', include('django_registration.urls', namespace = 'registration')),
-    url(r'^accounts/', include('accounts.urls', namespace = 'accounts')),
+    url(r'^register/', include('django_registration.urls', namespace='registration')),
+    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
 )
 
 # TODO: remove before deployment
