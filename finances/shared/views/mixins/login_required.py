de313df8a46d4011aa54e7329ed4242b8b6dcc64
Aaron Krebs <akrebs@ualberta.ca>
2013-03-29 18:52:04 -0600

    Fixed LoginRequiredMixin to inherit from object. This way it's not an old-style class, which was not intended at all.

--- finances/shared/views/mixins/login_required.py
+++ finances/shared/views/mixins/login_required.py
@@ -17,7 +17,7 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
 
-class LoginRequiredMixin():
+class LoginRequiredMixin(object):
     @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super(LoginRequiredMixin, self).dispatch(*args, **kwargs)
+    def dispatch(self, request, *args, **kwargs):
+        return super(LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
