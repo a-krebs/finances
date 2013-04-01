51f2469450d8775f5f217d6106f8fb0075c99ee1
Aaron Krebs <akrebs@ualberta.ca>
2013-03-31 20:59:07 -0600

    Removed most property decorators and protected attributes. These were mostly not necessary and really just over-engineered design.

--- finances/shared/models.py
+++ finances/shared/models.py
@@ -27,33 +27,10 @@ class OwnedModel(models.Model):
     Owner should be set at instantiation, and not be changed afterward.
     """
     
-    _owner = models.ForeignKey(User)
+    owner = models.ForeignKey(User)
     
     class Meta:
         abstract = True
-    
-    def __unicode__(self):
-        # this is an abstract model, so this method should be overridden later
-        raise NotImplementedError()
-    
-    @property
-    def owner(self):
-        """
-        Returns the User object of this Account's owner.
-        """
-        return self._owner
-    
-    @owner.setter
-    def owner(self, owner):
-        """
-        Raises a FieldError if this object's owner is already set.
-        """
-        # try to fetch _owner, if it doesn't exist we can set it
-        try:
-            if self._owner:
-                raise FieldError('owner is already set and cannot be set again')
-        except User.DoesNotExist:
-            self._owner = owner
     
     def is_allowed(self, request):
         """
@@ -74,28 +51,10 @@ class NamedModel(models.Model):
     Abstract class to move name attribute into a common parent class.
     """
     
-    _name = models.CharField(max_length=CHARFIELD_MAX_LENGTH)
+    name = models.CharField(max_length=CHARFIELD_MAX_LENGTH)
     
     class Meta:
         abstract = True
-    
-    def __unicode__(self):
-        # this is an abstract model, so this method should be overridden later
-        raise NotImplementedError()
-    
-    @property
-    def name(self):
-        """
-        Returns the display name of this Account.
-        """
-        return self._name
-    
-    @name.setter
-    def name(self, name):
-        """
-        Sets the display name of this Account.
-        """
-        self._name = name
 
 
 class PeriodLength(NamedModel):
@@ -109,6 +68,7 @@ class PeriodLength(NamedModel):
         # This is an abstract class, so this method should be implemented in subclasses
         raise NotImplementedError()
     
+    @classmethod
     @property
     def current_period_start_date(self):
         """
@@ -121,6 +81,7 @@ class PeriodLength(NamedModel):
         
         raise NotImplementedError()
     
+    @classmethod
     @property
     def current_period_end_date(self):
         """
@@ -132,6 +93,7 @@ class PeriodLength(NamedModel):
         """
         raise NotImplementedError()
     
+    @classmethod
     def in_current_period(self, timezone_date):
         """
         Returns True if the given timezone_date is in the current period, otherwise returns False
@@ -152,6 +114,7 @@ class Month(PeriodLength):
     def __unicode__(self):
         return "Month PeriodLength"
     
+    @classmethod
     @property
     def current_period_start_date(self):
         """
@@ -160,6 +123,7 @@ class Month(PeriodLength):
         """
         raise NotImplementedError()
     
+    @classmethod
     @property
     def current_period_end_date(self):
         """
@@ -168,6 +132,7 @@ class Month(PeriodLength):
         """
         raise NotImplementedError()
     
+    @classmethod
     def in_current_period(self, timezone_date):
         """
         Returns True if the given timezone_date is in the current period, otherwise returns False
@@ -184,6 +149,7 @@ class Year(PeriodLength):
     def __unicode__(self):
         return "Year PeriodLength"
     
+    @classmethod
     @property
     def current_period_start_date(self):
         """
@@ -216,8 +182,8 @@ class Budget(NamedModel, OwnedModel):
     with said Category are counted against the budget.
     """
     
-    _period_budget_amount = models.DecimalField(max_digits=15, decimal_places=2)
-    _period_length = models.ForeignKey(PeriodLength)
+    period_budget_amount = models.DecimalField(max_digits=15, decimal_places=2)
+    period_length = models.ForeignKey(PeriodLength)
     
     def __unicode__(self):
         return self.owner.user.username + "'s Budget: " + self.name
@@ -228,36 +194,6 @@ class Budget(NamedModel, OwnedModel):
         current period. Uses the @property decorator.
         """
         raise NotImplementedError()
-    
-    @property
-    def period_length(self):
-        """
-        Returns the PeriodLength object that determines the period length for this budget.
-        """
-        return self._period_length
-    
-    @period_length.setter
-    def period_length(self, length):
-        """
-        Given a PeriodLength object, sets the length of the period for this budget.
-        """
-        self._period_length = length
-    
-    @property
-    def period_budget_amount(self):
-        """
-        Returns the float value of the amount that is to be budgeted for each
-        budget period. Uses the @property decorator.
-        """
-        return self._period_budget_amount
-    
-    @period_budget_amount.setter
-    def period_budget_amount(self, float_amount):
-        """
-        Sets the float value of the amount that is to be budgeted for each
-        budget period. Uses the @property decorator.
-        """
-        self._period_budget_amount = float_amount
 
 
 class Category(NamedModel, OwnedModel):
@@ -267,24 +203,10 @@ class Category(NamedModel, OwnedModel):
     count against that Budget.
     """
     
-    _budget = models.ForeignKey(Budget)
+    budget = models.ForeignKey(Budget)
     
     def __unicode__(self):
         return self.owner.user.username + "'s Category: " + self.name
-    
-    @property
-    def budget(self):
-        """
-        Returns the Budget object that this Category is under.
-        """
-        return self._budget
-    
-    @budget.setter
-    def budget(self, budget):
-        """
-        Sets the Budget object that this Category is under.
-        """
-        self._budget = budget
 
 
 class RealAcct(OwnedModel, NamedModel):
@@ -312,55 +234,12 @@ class VirtualAcct(OwnedModel, NamedModel):
     a RealAcct class object to represent a portion of that account's aggregate balance.
     """
     
-    _parent_budget = models.ForeignKey(Budget)
-    _real_acct = models.ForeignKey(RealAcct)
+    parent_budget = models.ForeignKey(Budget)
+    real_acct = models.ForeignKey(RealAcct)
     
     def __unicode__(self):
         return self.owner.user.username + "'s VirtualAcct " + self.name
     
-    @property
-    def parent_budget(self):
-        """
-        Returns the budget object with which this virtual account is associated.
-        Uses the @property decorator.
-        """
-        return self._parent_budget
-    
-    @parent_budget.setter
-    def parent_budget(self, budget):
-        """
-        Raises a FieldError if the object's parent_budget is already set.
-        """
-        # try to fetch _parent_budget, if it doesn't exist we can set it
-        try:
-            if self._parent_budget:
-                raise FieldError('_parent_budget is already set and cannot be set again')
-        except RealAcct.DoesNotExist:
-            self._parent_budget = budget
-    
-    @parent_budget.setter
-    def parent_budget(self, budget):
-        raise NotImplementedError()
-    
-    @property
-    def real_acct(self):
-        """
-        Returns the RealAcct object this object is associated with.
-        """
-        raise NotImplementedError()
-    
-    @real_acct.setter
-    def real_acct(self, acct):
-        """
-        Raises a FieldError if this object's real_acct is already set.
-        """
-        # try to fetch _real_acct, if it doesn't exist we can set it
-        try:
-            if self._real_acct:
-                raise FieldError('_real_acct is already set and cannot be set again')
-        except RealAcct.DoesNotExist:
-            self._real_acct = acct
-            
     @property
     def balance(self):
         """
@@ -378,75 +257,12 @@ class RealTxn(OwnedModel):
     (money removed from account) as negative.
     """
     
-    _real_account = models.ForeignKey(RealAcct)
-    _value = models.DecimalField(max_digits=15, decimal_places=2)
-    _category = models.ForeignKey(Category)
+    real_account = models.ForeignKey(RealAcct)
+    value = models.DecimalField(max_digits=15, decimal_places=2)
+    category = models.ForeignKey(Category)
     
     def __unicode__(self):
         return self.owner.username.name + "'s RealTxn " + self.name
-    
-    @property
-    def value(self):
-        """
-        Returns the value of the transaction.
-        
-        Credit values (money into account) are given as positive; Debit
-        values (money removed from account) as negative.
-        
-        Uses the @property decorator.
-        """
-        raise NotImplementedError()
-    
-    @value.setter
-    def value(self, amount):
-        """
-        Sets the value of the transaction.
-        
-        Credit values (money into account) are given as positive; Debit
-        values (money removed from account) as negative.
-        
-        Uses the @property decorator.
-        """
-        raise NotImplementedError()
-    
-    @property
-    def category(self):
-        """
-        Returns the Category object associated with the Transaction.
-        
-        Uses the @property decorator.
-        """
-        raise NotImplementedError()
-    
-    @category.setter
-    def category(self, category):
-        """
-        Sets the Category this transaction is associated with.
-        
-        Uses the @property decorator.
-        """
-        raise NotImplementedError()
-    
-    @property
-    def real_acct(self):
-        """
-        Returns the Account object this transaction is counted against.
-        
-        Uses the @property decorator.
-        """
-        raise NotImplementedError()
-    
-    @real_acct.setter
-    def real_acct(self, acct):
-        """
-        Raises a FieldError if this object's real_acct is already set.
-        """
-        # try to fetch _real_acct, if it doesn't exist we can set it
-        try:
-            if self._real_acct:
-                raise FieldError('_real_acct is already set and cannot be set again')
-        except RealAcct.DoesNotExist:
-            self._real_acct = acct
 
 
 class VirtualTxn(OwnedModel):
@@ -457,74 +273,9 @@ class VirtualTxn(OwnedModel):
     (money removed from account) as negative.
     """
     
-    _virtual_acct = models.ForeignKey(VirtualAcct)
-    _value = models.DecimalField(max_digits=15, decimal_places=2)
-    _real_txn = models.ForeignKey(RealTxn)
+    virtual_acct = models.ForeignKey(VirtualAcct)
+    value = models.DecimalField(max_digits=15, decimal_places=2)
+    real_txn = models.ForeignKey(RealTxn)
     
     def __unicode__(self):
         return self.owner.user.username + "'s VirtualTxn " + self.name
-    
-    @property
-    def value(self):
-        """
-        Returns the value of the transaction.
-        
-        Credit values (money into account) are given as positive; Debit
-        values (money removed from account) as negative.
-        
-        Uses the @property decorator.
-        """
-        raise NotImplementedError()
-    
-    @value.setter
-    def value(self, amount):
-        """
-        Sets the value of the transaction.
-        
-        Credit values (money into account) are given as positive; Debit
-        values (money removed from account) as negative.
-        
-        Uses the @property decorator.
-        """
-        raise NotImplementedError()
-    
-    @property
-    def virtual_acct(self):
-        """
-        Returns the VirtualAcct object this transaction is counted against.
-        
-        Uses the @property decorator.
-        """
-        raise NotImplementedError()
-    
-    @virtual_acct.setter
-    def virtual_acct(self, acct):
-        """
-        Raises a FieldError if this object's virtual_acct is already set.
-        """
-        # try to fetch _virtual_acct, if it doesn't exist we can set it
-        try:
-            if self._virtual_acct:
-                raise FieldError('_virtual_acct is already set and cannot be set again')
-        except VirtualAcct.DoesNotExist:
-            self._virtual_acct = acct
-    
-    @property
-    def real_txn(self):
-        """
-        Returns the RealTxn object associated with this transaction. Uses
-        the @property decorator.
-        """
-        raise NotImplementedError()
-    
-    @real_txn.setter
-    def real_txn(self, txn):
-        """
-        Raises a FieldError if this object's real_txn is already set.
-        """
-        # try to fetch _real_txn, if it doesn't exist we can set it
-        try:
-            if self._real_txn:
-                raise FieldError('_real_txn is already set and cannot be set again')
-        except VirtualAcct.DoesNotExist:
-            self._real_txn = txn
